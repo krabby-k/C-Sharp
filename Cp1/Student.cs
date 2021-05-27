d1b4f42c0172bd7805b30266ac3ea80c1fc510e2
brian <briankin86@gmail.com>
2021-05-27 17:02:14 +0300

    changes to class student

--- Cp1/Student.cs
+++ Cp1/Student.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Cp1
 {
     class Student
     {
+        public static SqlConnection con = null;
+        public void myconnect()
+        {
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\brian\source\repos\krabby-k\C-sharp\Cp1\Db.mdf;Integrated Security=True");
+            con.Open();
+        }
     }
 }
